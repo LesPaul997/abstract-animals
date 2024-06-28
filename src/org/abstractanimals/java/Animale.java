ca6c3a9bff776524b2a7c3a7a0df19a32fdeb5af
Mario Giordano <164010608+LesPaul997@users.noreply.github.com>
2024-06-28 20:30:02 +0200

    add class abstract "Animale"

---
+++ src/org/abstractanimals/java/Animale.java
@@ -0,0 +1,12 @@
+package org.abstractanimals.java;
+
+public abstract class Animale {
+	
+	public void dormi() {
+		System.out.println("Zzz");
+	}
+	
+	public abstract void verso();
+	
+	public abstract void mangia();
+}
