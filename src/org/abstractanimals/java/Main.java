f736fd83aacf8263c85fab545c286b0aecf5ccd5
Mario Giordano <164010608+LesPaul997@users.noreply.github.com>
2024-06-28 21:04:24 +0200

    feat class "Main"

--- src/org/abstractanimals/java/Main.java
+++ src/org/abstractanimals/java/Main.java
@@ -3,8 +3,43 @@ package org.abstractanimals.java;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// Instanza e sottoclassi della classe Animale
+		Cane scotty = new Cane();
+		Passerotto benny = new Passerotto();
+		Delfino curioso = new Delfino();
+		Aquila peppe = new Aquila();
+		
+		scotty.verso();
+		scotty.mangia();
+		scotty.dormi();
+		
+		benny.verso();
+		benny.mangia();
+		benny.dormi();
+		
+		curioso.verso();
+		curioso.mangia();
+		curioso.dormi();
+		
+		peppe.verso();
+		peppe.mangia();
+		peppe.dormi();
+		
+		deveNuotare(scotty);
+		deveNuotare(curioso);
+		deveVolare(benny);
+		deveVolare(peppe);
+		
 	}
+	
+	public static void deveNuotare(INuotante animale) {
+		animale.nuota();
+	}
+	
+	public static void deveVolare(IVolante animale) {
+		animale.vola();
+	}
+	
+	
 
 }
